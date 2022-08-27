493dea9dd013b2bba08f0e46be381a49b5455fd2
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-08-27 16:21:58 +0200

    add daisy ui

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import * as THREE from 'three';
 import {
   generateGeometriesByNumberOfColors
 } from "colors2geometries";
-import './App.css';
 
 function App() {
   const ref = useRef<HTMLImageElement>(null);
+  const [loading, setLoading] = useState<boolean>(false);
   const [meshes, setMeshes] = useState<THREE.Mesh[]>([]);
 
   function loadImage(event: React.ChangeEvent<HTMLInputElement>) {
     if(event && event.target && event.target.files && ref.current) {
+      setLoading(true);
       ref.current.src = URL.createObjectURL(event.target.files[0]);
       ref.current.onload =  (event: any) => {
         if(!ref.current) {
@@ -20,7 +21,7 @@ function App() {
         const meshes = generateGeometriesByNumberOfColors(ref.current.id, 10);
         console.log(meshes);
         setMeshes(meshes);
-
+        setLoading(false);
       };
     }
   }
@@ -29,10 +30,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input type='file' accept="image/*" className="hidden" onChange={loadImage} />
-        <img /*className="hidden"*/ id="imageSrc" alt="No Image" ref={ref} />
+        <input type='file' accept="image/*" /*className="hidden"*/ onChange={loadImage} />
+        <img className="hidden" id="imageSrc" alt="No Image" ref={ref} />
         <canvas id="canvasOutput" />
-        <ThreeCanvas meshes={meshes} width={500} height={500} />
+        {
+          loading ?
+            <button className="btn loading">loading</button>
+          : <ThreeCanvas meshes={meshes} width={500} height={500} />
+        }
       </header>
     </div>
   );
