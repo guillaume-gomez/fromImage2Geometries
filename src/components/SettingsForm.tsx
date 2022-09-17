a537eb4c32c698dfd4e237eee20068390004fbc9
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-09-18 00:28:50 +0200

    collapse options

--- src/components/SettingsForm.tsx
+++ src/components/SettingsForm.tsx
@@ -41,8 +41,7 @@ function SettingsForm({
   const [loading, setLoading] = useState<boolean>(false);
   const [imageData, setImageData] = useState<string|null>(null);
   const [uploadOption, setUploadOption] = useState<boolean>(true);
-  const [hasEditable, setHasEditable] = useState<boolean>(false);
-  const [showGroupsZ, setShowGroupZ] = useState<boolean>(true);
+  const [showOptions, setShowOptions] = useState<boolean>(true);
 
   async function loadImageFromUrl(event: React.ChangeEvent<HTMLInputElement>) {
     const data = await toObjectUrl(event.target.value);
@@ -78,6 +77,7 @@ function SettingsForm({
   return (
     <div className="lg:absolute md:static lg:top-8 lg:left-8 lg:max-w-xs md:max-w-full md:w-full">
       <div className="overflow-auto card bg-base-100 shadow-2xl w-full" style={{maxHeight: "50vh"}}>
+
         <div className="card-body p-3 flex flex-col gap-5">
           <div className="flex flex-col bg-neutral-focus gap-2 items-center rounded p-2">
             <div className="flex items-center gap-2 self-end">
@@ -115,12 +115,22 @@ function SettingsForm({
             <button className="btn btn-primary" onClick={generate} disabled={imageData === null}>Generate 🧪</button>
             {error && <p className="text-error text-xs">{error}</p>}
           </div>
+
           <span className="text-xs">Double click to switch to fullscreen</span>
-          <div className="bg-neutral-focus rounded p-2 flex flex-col">
-            { loading ?
+
+          <div
+            tabIndex={0}
+            className={"collapse collapse-arrow border bg-neutral-focus border-0 rounded"}
+          >
+            <input type="checkbox" checked={showOptions} onChange={() => setShowOptions(!showOptions)}/>
+            <div className="collapse-title text-lg font-medium">
+              Options
+            </div>
+            <div className="collapse-content">
+              { loading ?
                 <button className="btn loading">loading</button>
-              :
-               <>
+                :
+                 <>
                   <CustomRange
                     label={"Velocity"}
                     min={0}
@@ -129,12 +139,11 @@ function SettingsForm({
                     value={velocity}
                     onChange={(value) => setVelocity(value)}
                   />
-                <div className="flex flex-col gap-5">
-                  <button className="btn btn-outline btn-primary" onClick={alignGroup}>
-                    align all groups
-                  </button>
-                {
-                    showGroupsZ ?
+                  <div className="flex flex-col gap-5">
+                    <button className="btn btn-outline btn-primary" onClick={alignGroup}>
+                      align all groups
+                    </button>
+                    {
                       groups.map(group => {
                         return (
                             <CustomRange
@@ -147,17 +156,11 @@ function SettingsForm({
                             />
                         )
                       })
-                    :
-                    <button className="btn btn-square" onClick={() => setShowGroupZ(false)}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                      </svg>
-                      Hide
-                    </button>
-                }
-                </div>
-              </>
-            }
+                    }
+                  </div>
+                </>
+              }
+            </div>
           </div>
           <a ref={refAnchor} className="btn btn-secondary" onClick={() => saveImage(refAnchor)}>Take Screenshot 📷</a>
         </div>
