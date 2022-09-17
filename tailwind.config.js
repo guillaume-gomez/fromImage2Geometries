dce16e9b0157320b285572180f7e866ae06db366
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-09-18 00:27:27 +0200

    create custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      mytheme: {
+       "primary": "#FFBE0B",
+       "secondary": "#FB5607",
+       "accent": "#FF006E",
+       "neutral": "#8338EC",
+       "base-100": "#3A86FF",
+       "info": "#66C7FF",
+       "success": "#87D039",
+       "warning": "#E3D664",
+       "error": "#FF7070",
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
