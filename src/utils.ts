7a0bbc56549fc024e8a7f5df3b70ae8d3de3dc6d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-06-18 14:19:52 +0000

    add 2 more utils function to resize image base64

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,31 @@
 
-export function resizeImageCanvas(originCanvas: HTMLCanvasElement, targetCanvas: HTMLCanvasElement, expectedWidth: number, expectedHeight: number) {
+
+export function resizeImage(baseUrl64: string) : string {
+  const canvas = document.createElement('canvas');
+  fromDataUrlToCanvas(baseUrl64, canvas);
+  // max width
+  // max height
+  resizeImageCanvas(canvas, canvas, 200, 200);
+  canvas.toDataURL();
+}
+
+
+async function fromDataUrlToCanvas(baseUrl64: string, originCanvas: HTMLCanvasElement ) {
+  const context = originCanvas.getContext('2d');
+
+  if(!context) {
+    throw new Error("Cannot find context");
+  }
+
+  const image = new Image();
+  image.src = await new Promise(resolve => resolve(baseUrl64));
+  image.onload = () => {
+    context.drawImage(image, 0, 0);
+  };
+}
+
+
+function resizeImageCanvas(originCanvas: HTMLCanvasElement, targetCanvas: HTMLCanvasElement, expectedWidth: number, expectedHeight: number) {
   // resize image
   const canvasBuffer = document.createElement("canvas");
   const contextBuffer = canvasBuffer.getContext("2d");
